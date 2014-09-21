4866fa67770c0f5c0d688e301f807473be545a61
finals <finals@users.noreply.github.com>
2014-09-21 17:36:29 +0800

    linux2.6.32注释

---
+++ drivers/base/base.h
@@ -0,0 +1,147 @@
+
+/**
+ * struct bus_type_private - structure to hold the private to the driver core portions of the bus_type structure.
+ *
+ * @subsys - the struct kset that defines this bus.  This is the main kobject
+ * @drivers_kset - the list of drivers associated with this bus
+ * @devices_kset - the list of devices associated with this bus
+ * @klist_devices - the klist to iterate over the @devices_kset
+ * @klist_drivers - the klist to iterate over the @drivers_kset
+ * @bus_notifier - the bus notifier list for anything that cares about things
+ * on this bus.
+ * @bus - pointer back to the struct bus_type that this structure is associated
+ * with.
+ *
+ * This structure is the one that is the actual kobject allowing struct
+ * bus_type to be statically allocated safely.  Nothing outside of the driver
+ * core should ever touch these fields.
+ */
+struct bus_type_private {
+	struct kset subsys;         /* 总线类型内嵌kset, 对应/sys/bus/下的某个目录 */
+	struct kset *drivers_kset;  /* 指向该总线类型下drivers子目录， /sys/bus/###/drivers */
+	struct kset *devices_kset;  /* 指向该总线类型下devices子目录， /sys/bus/###/devices */
+	struct klist klist_devices; /* 设备链表表头 */
+	struct klist klist_drivers; /* 驱动链表表头 */
+	struct blocking_notifier_head bus_notifier;
+	unsigned int drivers_autoprobe:1; /* 为1表示允许驱动自动探测设备 */
+	struct bus_type *bus;   /* 指向总线类型 */
+};
+
+struct driver_private {
+	struct kobject kobj;      /* 内嵌的kobject */
+	struct klist klist_devices;   /* 这个驱动支持的设备链表表头 */
+	struct klist_node knode_bus;  /* 链接到所属总线类型的驱动链表的连接件 */
+	struct module_kobject *mkobj; /* 执行这个驱动对应的module_kobject描述符的指针 */
+	struct device_driver *driver; /* 指向相关联的device_driver结构的指针 */
+};
+#define to_driver(obj) container_of(obj, struct driver_private, kobj)
+
+
+/**
+ * struct class_private - structure to hold the private to the driver core portions of the class structure.
+ *
+ * @class_subsys - the struct kset that defines this class.  This is the main kobject
+ * @class_devices - list of devices associated with this class
+ * @class_interfaces - list of class_interfaces associated with this class
+ * @class_dirs - "glue" directory for virtual devices associated with this class
+ * @class_mutex - mutex to protect the children, devices, and interfaces lists.
+ * @class - pointer back to the struct class that this structure is associated
+ * with.
+ *
+ * This structure is the one that is the actual kobject allowing struct
+ * class to be statically allocated safely.  Nothing outside of the driver
+ * core should ever touch these fields.
+ */
+struct class_private {
+	struct kset class_subsys;   /* 内嵌kset对应/sys/class/下某个子目录 */
+	struct klist class_devices;  /* 类的设备链表的表头 */
+	struct list_head class_interfaces;   /* 类的接口链表的表头 */
+	struct kset class_dirs;  /* 用于保存将属于该类的虚拟设备挂入/sys/devices/子树 */
+	struct mutex class_mutex;  /* 保护设备链表、接口链表的互斥量 */
+	struct class *class;     /* 指向相关联的class结构指针 */
+};
+#define to_class(obj)	\
+	container_of(obj, struct class_private, class_subsys.kobj)
+
+/**
+ * struct device_private - structure to hold the private to the driver core portions of the device structure.
+ *
+ * @klist_children - klist containing all children of this device
+ * @knode_parent - node in sibling list
+ * @knode_driver - node in driver list
+ * @knode_bus - node in bus list
+ * @driver_data - private pointer for driver specific info.  Will turn into a
+ * list soon.
+ * @device - pointer back to the struct class that this structure is
+ * associated with.
+ *
+ * Nothing outside of the driver core should ever touch these fields.
+ */
+struct device_private {
+	struct klist klist_children;     /* 本设备的孩子链表的表头 */
+	struct klist_node knode_parent;  /* 链接到所属父设备的孩子链表连接件 */
+	struct klist_node knode_driver;  /* 链接到所绑定驱动的设备链表的链接件 */
+	struct klist_node knode_bus;     /* 链接到所属总线类型的设备链表的连接件 */
+	void *driver_data;       /* 指向驱动私有数据的指针 */
+	struct device *device;   /* 指向相关联的device结构指针 */
+};
+#define to_device_private_parent(obj)	\
+	container_of(obj, struct device_private, knode_parent)
+#define to_device_private_driver(obj)	\
+	container_of(obj, struct device_private, knode_driver)
+#define to_device_private_bus(obj)	\
+	container_of(obj, struct device_private, knode_bus)
+
+extern int device_private_init(struct device *dev);
+
+/* initialisation functions */
+extern int devices_init(void);
+extern int buses_init(void);
+extern int classes_init(void);
+extern int firmware_init(void);
+#ifdef CONFIG_SYS_HYPERVISOR
+extern int hypervisor_init(void);
+#else
+static inline int hypervisor_init(void) { return 0; }
+#endif
+extern int platform_bus_init(void);
+extern int system_bus_init(void);
+extern int cpu_dev_init(void);
+
+extern int bus_add_device(struct device *dev);
+extern void bus_probe_device(struct device *dev);
+extern void bus_remove_device(struct device *dev);
+
+extern int bus_add_driver(struct device_driver *drv);
+extern void bus_remove_driver(struct device_driver *drv);
+
+extern void driver_detach(struct device_driver *drv);
+extern int driver_probe_device(struct device_driver *drv, struct device *dev);
+static inline int driver_match_device(struct device_driver *drv,
+				      struct device *dev)
+{
+	return drv->bus->match ? drv->bus->match(dev, drv) : 1;
+}
+
+extern void sysdev_shutdown(void);
+
+extern char *make_class_name(const char *name, struct kobject *kobj);
+
+extern int devres_release_all(struct device *dev);
+
+extern struct kset *devices_kset;
+
+#if defined(CONFIG_MODULES) && defined(CONFIG_SYSFS)
+extern void module_add_driver(struct module *mod, struct device_driver *drv);
+extern void module_remove_driver(struct device_driver *drv);
+#else
+static inline void module_add_driver(struct module *mod,
+				     struct device_driver *drv) { }
+static inline void module_remove_driver(struct device_driver *drv) { }
+#endif
+
+#ifdef CONFIG_DEVTMPFS
+extern int devtmpfs_init(void);
+#else
+static inline int devtmpfs_init(void) { return 0; }
+#endif
